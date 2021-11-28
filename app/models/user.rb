f84db9459a4876087ae049a1be664febaefcc618
kotahorii <khorii.micron@gmail.com>
2021-11-28 14:55:06 +0900

    user controller updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
+  mount_uploader :image, ImageUploader
 end
