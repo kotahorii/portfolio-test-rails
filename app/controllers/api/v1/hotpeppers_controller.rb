95515f32d23e8aa0d86973d54f4f9e9f89ede320
kotahorii <khorii.micron@gmail.com>
2021-12-07 20:33:20 +0900

    hotpepper index action add

--- app/controllers/api/v1/hotpeppers_controller.rb
+++ app/controllers/api/v1/hotpeppers_controller.rb
@@ -1,6 +1,16 @@
 class Api::V1::HotpeppersController < ApplicationController
   require 'httpclient'
 
+  def index
+    base_url = "#{ENV['SECRET_KEY']}" + params[:key]
+    client = HTTPClient.new()
+    response = client.get(base_url)
+    json = JSON.parse(response.body)
+    puts response.status
+    puts response.body
+    render json: json
+  end
+
   def create
     base_url = "#{ENV['SECRET_KEY']}" + params[:key]
     client = HTTPClient.new()
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
       resources :favorites, only: %i[index create destroy]
       resources :comments, only: %i[index create destroy]
       resources :rates, only: %i[index create update]
-      resources :hotpeppers, only: %i[create]
+      resources :hotpeppers, only: %i[index create]
       resources :labels, only: %i[index create destroy]
       mount_devise_token_auth_for 'User', at: 'auth', controllers: {
         registrations: 'api/v1/auth/registrations'
